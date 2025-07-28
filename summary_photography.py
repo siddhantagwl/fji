d1cb59f7b0e0428d44a26d68c176292bc91d0c5d
Siddhant Agarwal <siddhantagwl29@gmail.com>
2025-07-28 13:36:16 +0200

    Refactor summary_of_photography_project_wise to improve efficiency and add filtering logic for dates and include overall flag

--- summary_photography.py
+++ summary_photography.py
@@ -61,50 +61,59 @@ def summary_of_photography(df: DataFrame):
     return df_photography_summary
 
 
-def summary_of_photography_project_wise(df: DataFrame):
+def summary_of_photography_project_wise(df: DataFrame, include_overall: str, start_date, end_date) -> DataFrame:
 
     cols = ["Photography_date", "Project_name", "Items", "Images", "Photographers", "Photostackers", "Retouchers"]
-
     df_photography_summary_project_wise = utils.get_empty_df(cols)
+
+    if include_overall == "n":
+        df = utils.filter_df_on_dates(df, start_date, end_date, config.COL_PHOTOGRAPHER_DATE)
+        df.reset_index(drop=True, inplace=True)
 
     for (photography_date, project_name), group in df.groupby([config.COL_PHOTOGRAPHER_DATE, config.COL_PROJECT_NAME]):
 
         if not photography_date:
             continue
 
-        total_items = 0
-        total_images = 0
+        # total_items = 0
+        # total_images = 0
+
+        # unmerge_start_val = group.loc[indx, config.COL_UNMERGE_START]
+        # if unmerge_start_val == config.TRANSFER_VALUE:
+        #     continue
+        # photographer_values = set(
+        #     group.loc[indx, [config.COL_PHOTOGRAPHER_1, config.COL_PHOTOGRAPHER_2, config.COL_PHOTOGRAPHER_3]].values
+        # )
+        # # if all same values of NA, CV, FJI in p1,p2,p3 then skip
+        # if len(photographer_values) == 1 and list(photographer_values)[0] in config.PHOTOGRAPHER_SIGN_CONST_VALUES:
+        #     continue
+
+        # Filter out rows to skip in one go for efficiency (efficiency logic to above)
+        skip_mask = (
+            (group[config.COL_UNMERGE_START] == config.TRANSFER_VALUE)
+            | (
+                group[[config.COL_PHOTOGRAPHER_1, config.COL_PHOTOGRAPHER_2, config.COL_PHOTOGRAPHER_3]]
+                .apply(lambda x: len(set(x)) == 1 and list(set(x))[0] in config.PHOTOGRAPHER_SIGN_CONST_VALUES, axis=1)
+            )
+        )
+        filtered_group = group[~skip_mask]
+
+        # Calculate totals efficiently
+        item_cols = [
+            config.COL_PHOTOGRAPHY,
+            config.COL_BESPOKE,
+            config.COL_CAPPED,
+            config.COL_PHOTOGRAPHY_TO_VARIANCE,
+            config.COL_SAMPLES_RESTAKE,
+        ]
+        # Calculate row sums for the item columns
+        row_sums = filtered_group[item_cols].sum(axis=1)
+        total_items = row_sums.sum()
+        total_images = (row_sums > 0).sum()
 
         all_photographer_names = get_all_photographer_names(group)
         all_photostackers_names = get_all_photostackers_names(group)
         all_retouchers_names = get_all_retoucher_names(group)
-
-        for indx, row in group.iterrows():
-            unmerge_start_val = group.loc[indx, config.COL_UNMERGE_START]
-            if unmerge_start_val == config.TRANSFER_VALUE:
-                continue
-
-            photographer_values = set(
-                group.loc[indx, [config.COL_PHOTOGRAPHER_1, config.COL_PHOTOGRAPHER_2, config.COL_PHOTOGRAPHER_3]].values
-            )
-
-            # if all same values of NA, CV, FJI in p1,p2,p3 then skip
-            if len(photographer_values) == 1 and list(photographer_values)[0] in config.PHOTOGRAPHER_SIGN_CONST_VALUES:
-                continue
-
-            # Now get the actual values of photopgraphy, bespoke etc
-            val_photography = group.loc[indx, config.COL_PHOTOGRAPHY]
-            val_bespoke = group.loc[indx, config.COL_BESPOKE]
-            val_capped = group.loc[indx, config.COL_CAPPED]
-            val_photovar = group.loc[indx, config.COL_PHOTOGRAPHY_TO_VARIANCE]
-            val_samples_reshoot = group.loc[indx, config.COL_SAMPLES_RESTAKE]
-
-            row_sum = sum([val_photography, val_bespoke, val_capped, val_photovar, val_samples_reshoot])
-
-            total_items += row_sum
-
-            if row_sum > 0:
-                total_images += 1
 
         df_photography_summary_project_wise.loc[len(df_photography_summary_project_wise)] = [
             photography_date,
